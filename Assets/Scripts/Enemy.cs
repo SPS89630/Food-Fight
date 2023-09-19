4e4a204a189c673f358c0bd58f396b73a9141760
Taireru Benaru <38292283+chairdev@users.noreply.github.com>
2023-09-19 15:54:26 +0200

    initial commit

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public int MaxHP = 100;
+    public int CurrentHP = 100;
+    public float speed = 10;
+    public float throwDistance = 5.0f;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Move until a distance of 1 from the player
+        if(Vector3.Distance(transform.position, PlayerController.Instance.transform.position) > throwDistance)
+        {
+            transform.LookAt(PlayerController.Instance.transform);
+            transform.position = Vector3.MoveTowards(transform.position, PlayerController.Instance.transform.position, speed * Time.deltaTime);
+        }
+        else
+        {
+            Debug.Log("Reached player!");
+        }
+    }
+}
