5e097f6a769e428d54555168984d9d0d3199424d
89972 <121228696+89972@users.noreply.github.com>
2023-10-31 13:46:21 +0100

    import foodstand

---
+++ Assets/FoodStand.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FoodStand : MonoBehaviour
+{
+    public FruitID[] fruits;
+    public int foodCount = 0;
+    public GameObject spawnPoint;
+
+    public int currentSpawnDelay = SecondsToFrames(5);
+    int spawnDelay = 5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(foodCount < 5)
+        {
+            if(currentSpawnDelay <= 0)
+            {
+                SpawnFruit();
+                currentSpawnDelay = spawnDelay;
+            }
+            else
+            {
+                currentSpawnDelay--;
+            }
+        }
+    }
+
+    void SpawnFruit()
+    {
+        int fruitIndex = Random.Range(0, fruits.Length);
+        Vector3 pos = new Vector3(spawnPoint.transform.position.x, spawnPoint.transform.position.y + 0.5f, spawnPoint.transform.position.z);
+        GameObject fruitObject = Instantiate(GameManager.Instance.fruits[fruitIndex].prefab, spawnPoint.transform.position, Quaternion.identity);
+
+        //GameObject fruit = Instantiate(fruits[fruitIndex].fruitPrefab, spawnPoint.transform.position, Quaternion.identity);
+        fruitObject.GetComponent<Fruit>().ID = fruits[fruitIndex];
+        foodCount++;
+    }
+
+    static int SecondsToFrames(int seconds)
+    {
+        return seconds * 60;
+    }
+}
